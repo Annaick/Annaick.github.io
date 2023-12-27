c037bd8b88e87443b7842718a619b2868a843cf8
Student Developer <annaick.tiana@gmail.com>
2023-12-27 07:51:00 +0300

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -50,7 +50,7 @@ export default function Header(prop){
                 <p className='header-presentation__name-before'>{message}</p>
                 <p className='header-presentation__name'><LinearGradient gradient={["to right", "#00afff, #00ff8f"]}>ANNAICK</LinearGradient></p>
                 <p className='header-presentation__name-after'>TIANA</p>
-                <Title className='lightText' style={{fontSize: '1rem', marginBottom: '30px', marginTop: '10px'}}>Front-end react eveloper & UI/UX Designer</Title>
+                <Title className='lightText' style={{fontSize: '1rem', marginBottom: '30px', marginTop: '10px'}}>Front-end react developer & UI/UX Designer</Title>
                 <Button className='download' icon={<DownloadOutlined></DownloadOutlined>} href='/annaick__CV.pdf' download classNames='fluent' type='primary' aria-label='click to download cv'>DOWNLOAD CV</Button>
                 <div className='background' style={{position: 'relative'}}>
                 <Goo>
